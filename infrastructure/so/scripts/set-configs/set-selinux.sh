5ecd72a46563ac9ad2bdcdd31db7a1c5c0ea7b44
mrsilvestrini <mrsilvestrini@hotmail.com>
2019-11-20 12:51:41 -0300

    feat: Update script set-selinux with new features

--- infrastructure/so/scripts/set-configs/set-selinux.sh
+++ infrastructure/so/scripts/set-configs/set-selinux.sh
@@ -1,8 +1,9 @@
 # Copy folder configs to /mnt/configs
 #lines for set string SELINUX=permissive in file
-bin/cat /etc/selinux/config
-/bin/mkdir /etc/selunux/old
-/bin/cp -p /etc/selunux/config /etc/sysconfig/network-scripts/old
-/bin/rm -f /etc/selunux/config
+/bin/cat /etc/selinux/config
+/bin/rm -rf /etc/selinux/old
+/bin/mkdir /etc/selinux/old
+/bin/cp -p /etc/selinux/config /etc/sysconfig/network-scripts/old
+/bin/rm -f /etc/selinux/config
 /bin/cp -p /mnt/configs/selinux/config /etc/selinux/
 
