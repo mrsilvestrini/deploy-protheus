7762634916b86cc0b1c86d8e52d6d93bf9fe43d7
mrsilvestrini <mrsilvestrini@hotmail.com>
2019-11-25 21:47:34 -0300

    feat: Update files with new infos

--- infrastructure/so/scripts/set-configs/set-mounts.sh
+++ infrastructure/so/scripts/set-configs/set-mounts.sh
@@ -1,1 +1,31 @@
 #lines for set mounts,my case use fstab in /etc/fstab
+echo 'Stop SMB Service...'
+/bin/systemctl stop smb
+/bin/systemctl stop nmb
+
+echo 'Check Status SMB Services...'
+/bin/systemctl status smb
+/bin/systemctl status nmb
+
+echo 'Umount Share...'
+/bin/umount your_file_system_share
+
+ECHO 'Backup Files...'
+/bin/mv /etc/fstab /etc/fstab_old
+
+echo 'Update Files...'
+/bin/cp -p /mnt/configs/mounts/fstab /etc/
+
+echo 'Start SMB Service...'
+/bin/systemctl start smb
+/bin/systemctl start nmb
+
+echo 'Check Status SMB Services...'
+/bin/systemctl status smb
+/bin/systemctl status nmb
+
+echo 'Mount Share...'
+/bin/mount your_file_system_share
+
+echo 'Test Mount...'
+/bin/ls -la your_file_system_share
