5edb290c2016f105132b03a3c36838c4fc374ef3
mrsilvestrini <mrsilvestrini@hotmail.com>
2019-12-02 21:32:23 -0300

    fix: Fix mkdir and mv

--- infrastructure/so/scripts/set-configs/set-ssh.sh
+++ infrastructure/so/scripts/set-configs/set-ssh.sh
@@ -7,15 +7,15 @@
 # private key (id_rsa): 600 (-rw-------)
 
 #After create a user, copy file /ssh/authorized_keys to /home/user/.ssh
-/bin/mkdir /home/your_user/.ssh
+/bin/mkdir -p /home/your_user/.ssh
 /bin/chmod 700 /home/your_user/.ssh
-/bin/cp -p  /mnt/configs/ssh/authorized_keys /home/your_user/.ssh
+/bin/cp -p /mnt/configs/ssh/authorized_keys /home/your_user/.ssh
 /bin/chmod 644 /home/your_user/.ssh/authorized_keys
 /bin/chown -R your_user:your_user /home/your_user/.ssh/
 
 #set sshd_config
-/bin/mv -p  /etc/ssh/sshd_config /etc/ssh/sshd_config_bkp
-/bin/cp -p  /mnt/configs/ssh/sshd_config /etc/ssh/
+/bin/mv /etc/ssh/sshd_config /etc/ssh/sshd_config_bkp
+/bin/cp -p /mnt/configs/ssh/sshd_config /etc/ssh/
 
 #restart ssh
 /bin/systemctl start sshd.service
